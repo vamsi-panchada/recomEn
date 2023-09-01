8341872206a660385d8c660a9970f981696f558f
Panchada Vamsi <panchada@crayondata.com>
2023-09-01 19:40:34 +0530

    hotels recom modified

--- djangoProject/catalyst/views.py
+++ djangoProject/catalyst/views.py
@@ -10,11 +10,17 @@ def getRecom1(user_id, loc):
     # os.system('pwd')
     df_cust = pd.read_csv(os.path.join(settings.BASE_DIR, 'category_pattern.csv'))
     df_loc = pd.read_csv(os.path.join(settings.BASE_DIR, 'mapping_btw_local_trenda_and_coupons.csv'))
+    df_hot = pd.read_csv(os.path.join(settings.BASE_DIR, 'offers_hotels.csv'))
+    print(df_hot)
     user_cat = df_cust[df_cust['USER_ID']==user_id].sort_values('rank', ascending=False)['MERCHANT_CATEGORY'].to_list()
     user_cat = user_cat[0: len(user_cat) if len(user_cat)<=3 else 4]
     merchs = df_loc[df_loc['LOCATION']==loc]
     merchs = merchs[merchs['MERCHANT_CATEGORY'].isin(user_cat)]
-    return json.loads(merchs.groupby('MERCHANT_CATEGORY').head(2).reset_index(drop=True)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']].to_json())
+    resp = merchs.groupby('MERCHANT_CATEGORY').head(2).reset_index(drop=True)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']]
+    resp = df_hot.sample(4)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']]._append(resp, ignore_index=True)
+    # print(df_hot)
+    # print(resp)
+    return json.loads(resp.to_json())
 
 def index(request):
     data = {
@@ -29,6 +35,6 @@ def detail(request):
     id = request.GET.get('id')
     location = request.GET.get('location')
 
-    print(getRecom1(int(id),location))
+    # print(getRecom1(int(id),location))
 
     return JsonResponse(getRecom1(int(id),location), safe=False)
