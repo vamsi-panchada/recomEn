84b5bdc149d4344e7159c9cfb1847b89b74f0f4c
Parkhiya Dixitkumar Arvindbhai <parkhiya@crayondata.com>
2023-09-01 16:39:00 +0530

    new commit

--- djangoProject/catalyst/models.py
+++ djangoProject/catalyst/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+
+class transactions(models.Model):
+    transactionId
+    userId
+    merchantId
+    amount
+class merchantDetails(models.Model):
+    merchantId
+    merchantName
+    catogary
+    location
+
+class offers(models.Model):
+    offerId
+    merchantId
+    coupenCode
+    description
+
+class userDetails(models.Model):
+    userId=models.
+    userName
+    userAge
+    userIncome
+    userHometown
+
+class Disease(models.Model):
+    disname = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.disname
+
+
+class Record(models.Model):
+    aadharp = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patientaadhar', to_field='aadhar_no')
+    aadhard = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='docpatient')
+    disid = models.ForeignKey(Disease, on_delete=models.CASCADE, related_name='disease')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created)
+
+
+class Symptom(models.Model):
+    symptom = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.symptom
+
+
+class Relation(models.Model):
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE, related_name='Disease')
+    symptom = models.ForeignKey(Symptom, on_delete=models.CASCADE, related_name='Symptom')
+
+    def __str__(self):
+        return str(self.symptom) + " : "+str(self.disease)
+
+
+
--- djangoProject/catalyst/views.py
+++ djangoProject/catalyst/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 
+import pandas as pd
 
+def getRecom1(user_id, loc):
+    df_cust = pd.read_csv('category_pattern.csv')
+    df_loc = pd.read_csv('mapping_btw_local_trenda_and_coupons.csv')
+    user_cat = df_cust[df_cust['USER_ID']==user_id].sort_values('rank', ascending=False)['MERCHANT_CATEGORY'].to_list()
+    user_cat = user_cat[0: len(user_cat) if len(user_cat)<=3 else 4]
+    merchs = df_loc[df_loc['LOCATION']==loc]
+    merchs = merchs[merchs['MERCHANT_CATEGORY'].isin(user_cat)]
+    return merchs.groupby('MERCHANT_CATEGORY').head(2).reset_index(drop=True)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']].to_json()
 
 def index(request):
     data = {
@@ -14,13 +23,6 @@ def index(request):
 
 def detail(request):
     id = request.GET.get('id')
-    name = request.GET.get('name')
-    gender = request.GET.get('gender')
+    location = request.GET.get('location')
 
-    user = {
-        'id': id,
-        'name': name,
-        'gender': gender
-    }
-
-    return JsonResponse(user)
+    return JsonResponse(getRecom1(id,location))
