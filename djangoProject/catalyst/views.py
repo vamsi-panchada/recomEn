4b7879e046351ab0a3aebe274793481fc92d74e7
vamsi panchada <vamsivs58@gmail.com>
2023-09-02 22:01:08 +0530

    hotel range

--- djangoProject/catalyst/views.py
+++ djangoProject/catalyst/views.py
@@ -10,6 +10,7 @@ def getRecom1(user_id, loc):
     # os.system('pwd')
     df_cust = pd.read_csv(os.path.join(settings.BASE_DIR, 'category_pattern.csv'))
     df_loc = pd.read_csv(os.path.join(settings.BASE_DIR, 'mapping_btw_local_trenda_and_coupons.csv'))
+    df_user = pd.read_csv(os.path.join(settings.BASE_DIR, 'USER.csv'))
     df_hot = pd.read_csv(os.path.join(settings.BASE_DIR, 'offers_hotels.csv'))
     print(df_hot)
     user_cat = df_cust[df_cust['USER_ID']==user_id].sort_values('rank', ascending=False)['MERCHANT_CATEGORY'].to_list()
@@ -17,6 +18,8 @@ def getRecom1(user_id, loc):
     merchs = df_loc[df_loc['LOCATION']==loc]
     merchs = merchs[merchs['MERCHANT_CATEGORY'].isin(user_cat)]
     resp = merchs.groupby('MERCHANT_CATEGORY').head(2).reset_index(drop=True)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']]
+    user_income = df_user[df_user['user_id']==user_id]['user_income'].values[0]
+    df_hot =df_hot[df_hot['USER_MIN_RANGE']<=user_income][df_hot['USER_MAX_RANGE']>=user_income]
     # resp = df_hot.sample(4)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']]._append(resp, ignore_index=True)
     resp = pd.concat([df_hot.sample(4)[['MERCHANT_CATEGORY', 'MERCHANT_NAME', 'coupon', 'desc']], resp], axis=0, ignore_index=True)
     # print(df_hot)
